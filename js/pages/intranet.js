30eaacf012b3651395b21ef4ef448f5aa213a946
oscarricardosan <oscarricardosan@gmail.com>
2017-10-10 00:05:23 -0500

    Pruebas para background processes

--- js/pages/intranet.js
+++ js/pages/intranet.js
@@ -19,6 +19,18 @@ $(document).ready(function(){
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     window.open = cordova.InAppBrowser.open;
+    cordova.plugins.backgroundMode.setEnabled(true);
+    cordova.plugins.backgroundMode.overrideBackButton();
+
+    cordova.plugins.backgroundMode.setDefaults({
+        title: 'Courier App',
+        text: 'Me estoy ejecutando',
+        //icon: 'icon',  this will look for icon.png in platforms/android/res/drawable|mipmap
+        color: 'F14F4D', // hex format like 'F14F4D'
+        resume: Boolean,
+        hidden: Boolean,
+        bigText: Boolean
+    })
 }
 
 $(document).bind("mobileinit", function(){
