88ef0772bfeb3eb5e1d8535c004521673a0a6829
oscarricardosan <oscarricardosan@gmail.com>
2017-10-10 21:34:10 -0500

    Pruebas código de barras

--- js/pages/pickups.js
+++ js/pages/pickups.js
@@ -100,16 +100,12 @@ $(document).ready(function(){
                 alert("Scanning failed: " + error);
             },
             {
-                preferFrontCamera : true, // iOS and Android
                 showFlipCameraButton : true, // iOS and Android
                 showTorchButton : true, // iOS and Android
-                torchOn: true, // Android, launch with the torch switched on (if available)
-                saveHistory: true, // Android, save scan history (default false)
-                prompt : "Place a barcode inside the scan area", // Android
+                torchOn: false, // Android, launch with the torch switched on (if available)
+                prompt : "Pon el código de barras dentro del cuadrado", // Android
                 resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-                orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-                disableAnimations : true, // iOS
+                orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
                 disableSuccessBeep: false // iOS and Android
             }
         );
