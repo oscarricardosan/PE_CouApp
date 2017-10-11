86b0ee7f7cc96c0a05682e1ab98d4f89a9018d43
oscarricardosan <oscarricardosan@gmail.com>
2017-10-11 14:36:30 -0500

    Prueba con impresora

--- js/pages/index.js
+++ js/pages/index.js
@@ -25,9 +25,7 @@ var App= new Vue({
     watch: {
         printer_device: function(device){
             window.DatecsPrinter.connect(device.address,
-                function() {
-                    printSomeTestText();
-                },
+                function() {},
                 function() {
                     alert(JSON.stringify(error));
                 }
@@ -77,52 +75,7 @@ function onDeviceReady() {
 
 
 
-function printSomeTestText() {
-    window.DatecsPrinter.printText("Print Test!", 'ISO-8859-1',
-        function() {
-            printMyImage();
-        }
-    );
-}
-
-function printMyImage() {
-    var image = new Image();
-    image.src = 'img/some_image.jpg';
-    image.onload = function() {
-        var canvas = document.createElement('canvas');
-        canvas.height = 100;
-        canvas.width = 100;
-        var context = canvas.getContext('2d');
-        context.drawImage(image, 0, 0);
-        var imageData = canvas.toDataURL('image/jpeg').replace(/^data:image\/(png|jpg|jpeg);base64,/, ""); //remove mimetype
-        window.DatecsPrinter.printImage(
-            imageData, //base64
-            canvas.width,
-            canvas.height,
-            1,
-            function() {
-                printMyBarcode();
-            },
-            function(error) {
-                alert(JSON.stringify(error));
-            }
-        )
-    };
-}
-
-function printMyBarcode() {
-    try{
-    window.DatecsPrinter.printBarcode(
-        75, //here goes the barcode type code
-        '13132498746313210584982011487', //your barcode data
-        function() {
-            alert('success!');
-        },
-        function() {
-            alert(JSON.stringify(error));
-        }
-    );
-    }catch (error){
-        alert(JSON.stringify(error));
-    }
+function printText() {
+    var text= prompt('Texto a imprimir');
+    window.DatecsPrinter.printText(text+"\n\n\n", 'ISO-8859-1');
 }
