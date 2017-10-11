fa7f3525c83fd99d7bbc1a5e30b8a1c81c234a17
oscarricardosan <oscarricardosan@gmail.com>
2017-10-11 15:51:37 -0500

    Prueba con impresora

--- js/pages/index.js
+++ js/pages/index.js
@@ -79,21 +79,21 @@ function printText() {
 
     alert(1);
     window.DatecsPrinter.feedPaper(1);
-    window.DatecsPrinter.printText('0___ 1 ', 'ISO-8859-1');
+    window.DatecsPrinter.printText('0___ 1 ', 'UTF-8');
     //window.DatecsPrinter.drawPageRectangle(x, y, width, height, fillMode, onSuccess, onError);
     window.DatecsPrinter.feedPaper(1);
     window.DatecsPrinter.drawPageRectangle(1, 0, 56, 20, 1, function(){}, function(){});
-    window.DatecsPrinter.printText('Acá 1', 'ISO-8859-1');
+    window.DatecsPrinter.printText('Acá 1', 'UTF-8');
 
     //(x, y, width, height, fillMode, thickness, onSuccess, onError
     window.DatecsPrinter.feedPaper(1);
     window.DatecsPrinter.drawPageFrame(1, 0, 56, 20, 1, 1, function(){}, function(){});
-    window.DatecsPrinter.printText('Acá 2', 'ISO-8859-1');
+    window.DatecsPrinter.printText('Acá 2', 'UTF-8');
 
     //(x, y, width, height, direction, onSuccess, onError)
     window.DatecsPrinter.feedPaper(1);
     window.DatecsPrinter.setPageRegion(1, 0, 56, 20, 1, function(){}, function(){});
-    window.DatecsPrinter.printText('Acá 3', 'ISO-8859-1');
+    window.DatecsPrinter.printText('Acá 3', 'UTF-8');
 
     window.DatecsPrinter.feedPaper(1);
     // window.DatecsPrinter.drawPageRectangle(0, 0, 58, 20, 0, function(){}, function(){});
