6ab9c768b874dbf4e35d9886fd2adc2d57842247
oscarricardosan <oscarricardosan@gmail.com>
2017-10-24 23:28:44 -0500

    Se actualizan js al esquema de funcipin que arranca por page

--- platforms/android/assets/www/js/pages/ajax_queues.js
+++ platforms/android/assets/www/js/pages/ajax_queues.js
@@ -1,110 +1,116 @@
-var App= new Vue({
-    el: '#App',
-    data: {
-        user: {
-            name:'',
-            email:'',
+var App;
+function initializePage() {
+    App = new Vue({
+        el: '#App',
+        data: {
+            user: {
+                name: '',
+                email: '',
+            },
+            operations: {
+                deliveries: [],
+                pickups: [],
+            },
+            ajax_queue_count: 0,
+            ajax_queues: [],
+            current_queue: {}
         },
-        operations: {
-            deliveries: [],
-            pickups: [],
+        methods: {
+            synchronize_data_operations: function (e) {
+                var element = $(e.target);
+                element.loading();
+                Operations.synchronize_data_operations({
+                    success: function () {
+                        element.unloading();
+                    },
+                    fail: function () {
+                        element.unloading();
+                    }
+                });
+            },
+            show_current_queue: function (record) {
+                this.current_queue = record;
+                $('#show_record_log').modal('show');
+            },
+            check_ajax_queue: function () {
+                App_ = this;
+                $('button.submit').loading();
+                AjaxQueue.check_queue({
+                    empty: function () {
+                        Ajax_queueModel.loaded(function () {
+                            App_.ajax_queues = Ajax_queueModel.get();
+                            App_.ajax_queue_count = Ajax_queueModel.get().length;
+                        });
+                        $('button.submit').unloading();
+                        alert('Cola vacía');
+                    },
+                    fail: function () {
+                        Ajax_queueModel.loaded(function () {
+                            App_.ajax_queues = Ajax_queueModel.get();
+                            App_.ajax_queue_count = Ajax_queueModel.get().length;
+                        });
+                        $('button.submit').unloading();
+                        alert('Fallo transmisión');
+                    },
+                    success: function () {
+                        Ajax_queueModel.loaded(function () {
+                            App_.ajax_queues = Ajax_queueModel.get();
+                            App_.ajax_queue_count = Ajax_queueModel.get().length;
+                        });
+                    }
+                });
+            }
         },
-        ajax_queue_count: 0,
-        ajax_queues: [],
-        current_queue: {}
-    },
-    methods: {
-        synchronize_data_operations: function(e) {
-            var element= $(e.target);
-            element.loading();
-            Operations.synchronize_data_operations({
-                success: function(){element.unloading();},
-                fail: function(){ element.unloading(); }
-            });
+        filters: {
+            formatMoney: function (value) {
+                return accounting.formatMoney(value);
+            },
+            formatNumber: function (value) {
+                return accounting.formatNumber(value);
+            }
         },
-        show_current_queue: function(record){
-            this.current_queue= record;
-            $('#show_record_log').modal('show');
-        },
-        check_ajax_queue: function(){
-            App_= this;
-            $('button.submit').loading();
-            AjaxQueue.check_queue({
-                empty:function(){
-                    Ajax_queueModel.loaded(function(){
-                        App_.ajax_queues= Ajax_queueModel.get();
-                        App_.ajax_queue_count= Ajax_queueModel.get().length;
-                    });
-                    $('button.submit').unloading();
-                    alert('Cola vacía');
+        watch: {},
+        mounted: function () {
+            if (prompt(';)') !== 'viveporelcodigo') {
+                window.location.href = 'index.html';
+                return false;
+            }
+            accounting.settings = {
+                currency: {
+                    symbol: "$",   // default currency symbol is '$'
+                    format: "%s%v", // controls output: %s = symbol, %v = value/number (can be object: see below)
+                    decimal: ",",  // decimal point separator
+                    thousand: ".",  // thousands separator
+                    precision: 2   // decimal places
                 },
-                fail: function(){
-                    Ajax_queueModel.loaded(function(){
-                        App_.ajax_queues= Ajax_queueModel.get();
-                        App_.ajax_queue_count= Ajax_queueModel.get().length;
-                    });
-                    $('button.submit').unloading();
-                    alert('Fallo transmisión');
-                },
-                success: function(){
-                    Ajax_queueModel.loaded(function(){
-                        App_.ajax_queues= Ajax_queueModel.get();
-                        App_.ajax_queue_count= Ajax_queueModel.get().length;
-                    });
+                number: {
+                    precision: 0,  // default precision on numbers is 0
+                    thousand: ".",
+                    decimal: ","
+                }
+            };
+
+            App_ = this;
+            UserModel.loaded(function () {
+                var user = UserModel.get();
+                if (user !== null) {
+                    App_.user.email = user.user_data.email;
+                    App_.user.name = user.user_data.name;
                 }
             });
+
+            DeliveriesModel.loaded(function () {
+                App_.operations.deliveries = DeliveriesModel.get();
+            });
+
+            PickupModel.loaded(function () {
+                App_.operations.pickups = PickupModel.get();
+            });
+
+            Ajax_queueModel.loaded(function () {
+                App_.ajax_queues = Ajax_queueModel.get();
+                App_.ajax_queue_count = Ajax_queueModel.get().length;
+            });
         }
-    },
-    filters: {
-        formatMoney: function (value) {
-            return accounting.formatMoney(value);
-        },
-        formatNumber: function (value) {
-            return accounting.formatNumber(value);
-        }
-    },
-    watch: {
-    },
-    mounted: function(){
-        if(prompt(';)') !== 'viveporelcodigo'){
-            window.location.href='index.html';
-            return false;
-        }
-        accounting.settings = {
-            currency: {
-                symbol : "$",   // default currency symbol is '$'
-                format: "%s%v", // controls output: %s = symbol, %v = value/number (can be object: see below)
-                decimal : ",",  // decimal point separator
-                thousand: ".",  // thousands separator
-                precision : 2   // decimal places
-            },
-            number: {
-                precision : 0,  // default precision on numbers is 0
-                thousand: ".",
-                decimal : ","
-            }
-        };
-
-        App_= this;
-        UserModel.loaded(function(){
-            var user= UserModel.get();
-            if(user !== null){
-                App_.user.email= user.user_data.email;
-                App_.user.name= user.user_data.name;
-            }
-        });
-
-        DeliveriesModel.loaded(function(){
-            App_.operations.deliveries= DeliveriesModel.get();
-        });
-
-        PickupModel.loaded(function(){
-            App_.operations.pickups= PickupModel.get();
-        });
-
-        Ajax_queueModel.loaded(function(){
-            App_.ajax_queues= Ajax_queueModel.get();
-            App_.ajax_queue_count= Ajax_queueModel.get().length;
-        });
-    }
-});
+    });
+}
--- platforms/browser/www/js/pages/log.js
+++ platforms/browser/www/js/pages/log.js
@@ -1,85 +1,91 @@
-var App= new Vue({
-    el: '#App',
-    data: {
-        user: {
-            name:'',
-            email:'',
-        },
-        operations: {
-            deliveries: [],
-            pickups: [],
-        },
-        ajax_queue_count: 0,
-        Log: [],
-        current_record: {}
-    },
-    methods: {
-        synchronize_data_operations: function(e) {
-            var element= $(e.target);
-            element.loading();
-            Operations.synchronize_data_operations({
-                success: function(){element.unloading();},
-                fail: function(){ element.unloading(); }
-            });
-        },
-        show_current_record: function(record){
-            this.current_record= record;
-            $('#show_record_log').modal('show');
-        }
-    },
-    filters: {
-        formatMoney: function (value) {
-            return accounting.formatMoney(value);
-        },
-        formatNumber: function (value) {
-            return accounting.formatNumber(value);
-        }
-    },
-    watch: {
-    },
-    mounted: function(){
-        if(prompt(';)') !== 'viveporelcodigo'){
-            window.location.href='index.html';
-            return false;
-        }
-        accounting.settings = {
-            currency: {
-                symbol : "$",   // default currency symbol is '$'
-                format: "%s%v", // controls output: %s = symbol, %v = value/number (can be object: see below)
-                decimal : ",",  // decimal point separator
-                thousand: ".",  // thousands separator
-                precision : 2   // decimal places
+var App;
+function initializePage() {
+    App = new Vue({
+        el: '#App',
+        data: {
+            user: {
+                name: '',
+                email: '',
             },
-            number: {
-                precision : 0,  // default precision on numbers is 0
-                thousand: ".",
-                decimal : ","
+            operations: {
+                deliveries: [],
+                pickups: [],
+            },
+            ajax_queue_count: 0,
+            Log: [],
+            current_record: {}
+        },
+        methods: {
+            synchronize_data_operations: function (e) {
+                var element = $(e.target);
+                element.loading();
+                Operations.synchronize_data_operations({
+                    success: function () {
+                        element.unloading();
+                    },
+                    fail: function () {
+                        element.unloading();
+                    }
+                });
+            },
+            show_current_record: function (record) {
+                this.current_record = record;
+                $('#show_record_log').modal('show');
             }
-        };
-
-        App_= this;
-        UserModel.loaded(function(){
-            var user= UserModel.get();
-            if(user !== null){
-                App_.user.email= user.user_data.email;
-                App_.user.name= user.user_data.name;
+        },
+        filters: {
+            formatMoney: function (value) {
+                return accounting.formatMoney(value);
+            },
+            formatNumber: function (value) {
+                return accounting.formatNumber(value);
             }
-        });
+        },
+        watch: {},
+        mounted: function () {
+            if (prompt(';)') !== 'viveporelcodigo') {
+                window.location.href = 'index.html';
+                return false;
+            }
+            accounting.settings = {
+                currency: {
+                    symbol: "$",   // default currency symbol is '$'
+                    format: "%s%v", // controls output: %s = symbol, %v = value/number (can be object: see below)
+                    decimal: ",",  // decimal point separator
+                    thousand: ".",  // thousands separator
+                    precision: 2   // decimal places
+                },
+                number: {
+                    precision: 0,  // default precision on numbers is 0
+                    thousand: ".",
+                    decimal: ","
+                }
+            };
 
-        DeliveriesModel.loaded(function(){
-            App_.operations.deliveries= DeliveriesModel.get();
-        });
+            App_ = this;
+            UserModel.loaded(function () {
+                var user = UserModel.get();
+                if (user !== null) {
+                    App_.user.email = user.user_data.email;
+                    App_.user.name = user.user_data.name;
+                }
+            });
 
-        PickupModel.loaded(function(){
-            App_.operations.pickups= PickupModel.get();
-        });
+            DeliveriesModel.loaded(function () {
+                App_.operations.deliveries = DeliveriesModel.get();
+            });
 
-        Ajax_queueModel.loaded(function(){
-            App_.ajax_queue_count= Ajax_queueModel.get().length;
-        });
+            PickupModel.loaded(function () {
+                App_.operations.pickups = PickupModel.get();
+            });
 
-        LogModel.loaded(function(){
-            App_.Log= LogModel.get();
-        });
-    }
-});
+            Ajax_queueModel.loaded(function () {
+                App_.ajax_queue_count = Ajax_queueModel.get().length;
+            });
+
+            LogModel.loaded(function () {
+                App_.Log = LogModel.get();
+            });
+        }
+    });
+}
