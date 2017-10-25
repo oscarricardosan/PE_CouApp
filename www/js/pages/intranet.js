00501885bbdc959f62c624dea174a6e80fda0f7d
oscarricardosan <oscarricardosan@gmail.com>
2017-10-25 10:28:54 -0500

    Pruebas obligar activación gps

--- www/js/pages/intranet.js
+++ www/js/pages/intranet.js
@@ -1,104 +1,109 @@
-function check_hardware(){
-    var Permissions = cordova.plugins.permissions;
-    var list = [
-        Permissions.BLUETOOTH,
-        Permissions.ACCESS_COARSE_LOCATION,
-        Permissions.ACCESS_FINE_LOCATION
-    ];
+try{
+    function check_hardware(){
+        var Permissions = cordova.plugins.permissions;
+        var list = [
+            Permissions.BLUETOOTH,
+            Permissions.ACCESS_COARSE_LOCATION,
+            Permissions.ACCESS_FINE_LOCATION
+        ];
 
-    Permissions.hasPermission(list, success, error);
+        Permissions.hasPermission(list, success, error);
 
-    function error() {
-        alert('GPS y Bluetooth deben estar activados.');
-        window.location.reload();
-    }
+        function error() {
+            alert('GPS y Bluetooth deben estar activados.');
+            window.location.reload();
+        }
 
-    function success( status ) {
-        if (!status.hasPermission) {
-            Permissions.requestPermissions(
-                list,
-                function (status) {
-                    if (!status.hasPermission) error();
-                },
-                error);
-        }else{
-            initializeIntranet();
+        function success( status ) {
+            if (!status.hasPermission) {
+                Permissions.requestPermissions(
+                    list,
+                    function (status) {
+                        if (!status.hasPermission) error();
+                    },
+                    error);
+            }else{
+                initializeIntranet();
+            }
         }
     }
-}
-
-/** Ready on mobiles **/
-document.addEventListener("deviceready", onDeviceReadyIntranet, false);
-function onDeviceReadyIntranet() {
-    check_hardware();
-}
-
-function initializeIntranet(){
-    $(document).ready(function(){
-        Login.is_logged_in(function(success, user){
-            if(!success){
-                alert('Debes iniciar sesión para continuar');
-                window.location.href= 'login.html';
-            }
-        });
-
-        $('.logout').click(function(event){
-            event.preventDefault();
-            Login.logout();
-        });
-    });
-
-
 
     /** Ready on mobiles **/
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-        window.open = cordova.InAppBrowser.open;
-
-        /** BACKGROUND PROCESS**/
-        var index_execution= 0;
-        cordova.plugins.backgroundMode.enable();
-        cordova.plugins.backgroundMode.disableWebViewOptimizations();
-        cordova.plugins.backgroundMode.overrideBackButton();
-
-        cordova.plugins.backgroundMode.setDefaults({
-            title: 'Courier App',
-            text: 'Me estoy ejecutando',
-            //icon: 'icon',  this will look for icon.png in platforms/android/res/drawable|mipmap
-            color: '#b3b3ff', // hex format like 'F14F4D'
-            resume: true,
-            hidden: false,
-            bigText: false
-        });
-
-        cordova.plugins.backgroundMode.on('activate', function () {
-            index_execution++;
-
-            /***
-             * @Notificaciones cada 100 segundos, cambio en la barra de mensaje
-             */
-            var index_execution=0;
-            setInterval(function () {
-                /*          if(index_execution % 100 == 0) {
-                              navigator.notification.beep(2);
-                              navigator.notification.vibrate([1500, 500, 1500]);
-                          }
-              */
-                cordova.plugins.backgroundMode.configure({
-                    text:
-                    "ENtrada "+index_execution
-                });
-            }, 5000);
-
-
-        });
-
-        /** CLOSE BACKGROUND PROCESS**/
+    document.addEventListener("deviceready", onDeviceReadyIntranet, false);
+    function onDeviceReadyIntranet() {
+        check_hardware();
     }
 
-    $(document).bind("mobileinit", function(){
-        $.mobile.allowCrossDomainPages = true;
-    });
+    function initializeIntranet(){
+        $(document).ready(function(){
+            Login.is_logged_in(function(success, user){
+                if(!success){
+                    alert('Debes iniciar sesión para continuar');
+                    window.location.href= 'login.html';
+                }
+            });
 
-    initializePage();
+            $('.logout').click(function(event){
+                event.preventDefault();
+                Login.logout();
+            });
+        });
+
+
+
+        /** Ready on mobiles **/
+        document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady() {
+            window.open = cordova.InAppBrowser.open;
+
+            /** BACKGROUND PROCESS**/
+            var index_execution= 0;
+            cordova.plugins.backgroundMode.enable();
+            cordova.plugins.backgroundMode.disableWebViewOptimizations();
+            cordova.plugins.backgroundMode.overrideBackButton();
+
+            cordova.plugins.backgroundMode.setDefaults({
+                title: 'Courier App',
+                text: 'Me estoy ejecutando',
+                //icon: 'icon',  this will look for icon.png in platforms/android/res/drawable|mipmap
+                color: '#b3b3ff', // hex format like 'F14F4D'
+                resume: true,
+                hidden: false,
+                bigText: false
+            });
+
+            cordova.plugins.backgroundMode.on('activate', function () {
+                index_execution++;
+
+                /***
+                 * @Notificaciones cada 100 segundos, cambio en la barra de mensaje
+                 */
+                var index_execution=0;
+                setInterval(function () {
+                    /*          if(index_execution % 100 == 0) {
+                                  navigator.notification.beep(2);
+                                  navigator.notification.vibrate([1500, 500, 1500]);
+                              }
+                  */
+                    cordova.plugins.backgroundMode.configure({
+                        text:
+                        "ENtrada "+index_execution
+                    });
+                }, 5000);
+
+
+            });
+
+            /** CLOSE BACKGROUND PROCESS**/
+        }
+
+        $(document).bind("mobileinit", function(){
+            $.mobile.allowCrossDomainPages = true;
+        });
+
+        initializePage();
+    }
+}catch (error){
+    alert(error);
+    alert(JSON.stringify(error));
 }
