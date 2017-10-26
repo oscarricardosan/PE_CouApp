354b181cfadaeb112bc181b734a9f7fbc6ef9a34
oscarricardosan <oscarricardosan@gmail.com>
2017-10-26 14:10:20 -0500

    Implementación background process

---
+++ www/js/app/ProcessBackground.js
@@ -0,0 +1,21 @@
+var ProcessBackground= (function () {
+
+    var index_executionBack= 0;
+    var run= function (){
+        //navigator.notification.vibrate([1000]);
+        index_executionBack++;
+        cordova.plugins.backgroundMode.configure({
+            text:
+            "ENtrada "+index_executionBack+"\n"+
+            "Primer ejecución "+(first_execution?'si':'no')
+        });
+        first_execution= false;
+    };
+
+    function construct(){//Funcion que controla cuales son los metodos publicos
+        return {
+            run            : run,
+        }
+    }
+    return {construct:construct};//retorna los metodos publicos
+})().construct();
