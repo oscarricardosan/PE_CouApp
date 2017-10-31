90070ab8e597b69a13b080d7a085b2bf0a54ff1e
oscarricardosan <oscarricardosan@gmail.com>
2017-10-31 12:08:53 -0500

    Pruebas GPS Background

--- www/js/pages/intranet.js
+++ www/js/pages/intranet.js
@@ -97,7 +97,7 @@ function initializeIntranet(){
                 title: 'Courier App',
                 text: 'Bienvenido',
                 icon: 'danger', // this will look for icon.png in platforms/android/res/drawable|mipmap
-                color: 'ff0000', // hex format like 'F14F4D'
+                color: '#ff0000', // hex format like 'F14F4D'
                 resume: true,
                 hidden: false,
                 bigText: false
