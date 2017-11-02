5d8486cb4bf431dafb1acc3763b5d6aba28f1d94
oscarricardosan <oscarricardosan@gmail.com>
2017-11-02 17:04:13 -0500

    Ajuste en formato

--- www/js/app/PrinterFormats.js
+++ www/js/app/PrinterFormats.js
@@ -54,6 +54,8 @@ var PrinterFormat= (function () {
         window.DatecsPrinter.feedPaper(2);
         window.DatecsPrinter.printText(' Recoleccion numero: '+App.operations.current_pickup.pickup_number, 'ISO-8859-1');
         window.DatecsPrinter.feedPaper(1);
+        window.DatecsPrinter.printText(' Contenido: '+accounting.formatMoney(App.operations.current_pickup.content), 'ISO-8859-1');
+        window.DatecsPrinter.feedPaper(1);
         window.DatecsPrinter.printText(' Valor: '+accounting.formatMoney(App.operations.current_pickup.value), 'ISO-8859-1');
         window.DatecsPrinter.feedPaper(1);
         window.DatecsPrinter.printText(' Firma cliente', 'ISO-8859-1');
@@ -94,6 +96,8 @@ var PrinterFormat= (function () {
         window.DatecsPrinter.printText('             SAVNE ', 'ISO-8859-1');
         window.DatecsPrinter.feedPaper(2);
         window.DatecsPrinter.printText(' Entrega numero: '+App.operations.current_delivery.delivery_number, 'ISO-8859-1');
+        window.DatecsPrinter.feedPaper(1);
+        window.DatecsPrinter.printText(' Contenido: '+accounting.formatMoney(App.operations.current_delivery.content), 'ISO-8859-1');
         window.DatecsPrinter.feedPaper(1);
         window.DatecsPrinter.printText(' Valor: '+accounting.formatMoney(App.operations.current_delivery.value), 'ISO-8859-1');
         window.DatecsPrinter.feedPaper(1);
