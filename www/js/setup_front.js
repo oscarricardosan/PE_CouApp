99bda560366579a19877f2321c6e32cc4482dfbd
oscarricardosan <oscarricardosan@gmail.com>
2017-12-04 16:29:21 -0500

    Optimizaciones

--- www/js/setup_front.js
+++ www/js/setup_front.js
@@ -1,15 +1,3 @@
-$(document).ready(function(){
-    $('.select2').select2();
-
-    //Date picker
-    $('.datepicker').datepicker({
-        autoclose: true,
-        format: 'yyyy-mm-dd'
-    });
-
-    //Timepicker
-    $(".timepicker").timepicker({
-        showInputs: false,
-        showMeridian: false,
-    });
+$(function() {
+    FastClick.attach(document.body);
 });
