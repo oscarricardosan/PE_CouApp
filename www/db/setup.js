7834d4b06018c5bf9c2def1568e65c5350f25569
oscarricardosan <oscarricardosan@gmail.com>
2017-12-18 20:08:45 -0500

    Se corrige página map.

--- www/db/setup.js
+++ www/db/setup.js
@@ -9,12 +9,5 @@ try{
             androidLockWorkaround: 1
         });
         Migrations.run();
-        var fdb = new ForerunnerDB(),
-            db = fdb.db("courier_app");
-        //db.persist.driver("IndexedDB"); //Calidad: 10 - 404kB
-        //db.persist.driver("WebSQL"); //Calidad: 9 - 344Kb
-        //db.persist.driver("LocalStorage"); //Calidad: 0 - 0
-
-
     });
 }catch(e){alert(e.message);}
--- www/js/pages/map.js
+++ www/js/pages/map.js
@@ -86,26 +86,26 @@ function initializePage() {
             date_to_filter: function(date){
                 var App_= this;
                 this.remove_markers_in_map();
-                this.deliveries_to_show= _.where(App_.operations.deliveries, {delivery_date: date});
-                this.pickups_to_show= _.where(App_.operations.pickups, {pickup_date: date});
+                this.deliveries_to_show= _.where(App_.operations.deliveries, {date: date});
+                this.pickups_to_show= _.where(App_.operations.pickups, {date: date});
             },
             pickups_to_show: function(pickups){
                 var App_= this;
                 var url_params= UrlUtility_.getParams();
                 $.each(pickups, function (index, pickup) {
-                    if(pickup.longitude !== null && pickup.latitude !== null) {
-                        var icon = App_.new_icon('images/map_icons/pickup_' + pickup.pickup_state.class + '.png?1');
+                    if(pickup.long !== null && pickup.lat !== null) {
+                        var icon = App_.new_icon('images/map_icons/pickup_'+Settings.pickup_state[pickup.state_id].class+'.png?1');
 
                         var marker = new L.marker().setLatLng({
                             lng: pickup.long,
                             lat: pickup.lat
                         }).setIcon(icon).addTo(App_.map).bindPopup(
-                            "<div style='text-align:center;'>Recolección <span class='label bg-" + pickup.pickup_state.class + "'>" + pickup.pickup_state.name + "</span></div>" +
-                            "<b>" + pickup.pickup_number + "</b><br>" +
+                            "<div style='text-align:center;'>Recolección <span class='label bg-"+Settings.pickup_state[pickup.state_id].class+"'>"+Settings.pickup_state[pickup.state_id].name+"</span></div>" +
+                            "<b>" + pickup.number + "</b><br>" +
                             "<b>Dirección: </b> " + pickup.address + " <br>" +
                             "<b>Observaciones dirección: </b> " + pickup.long_address + "<br>" +
                             "<i class='fa fa-clock-o'></i>" + pickup.start_time + " y " + pickup.end_time + " <br>" +
-                            "<i class='fa fa-globe'></i> A " + pickup.distance_in_mts + "  <br>" +
+                            "<i class='fa fa-globe'></i> A " + Haversine.mtrs_to_text(pickup.distance_in_mts) + "  <br>" +
                             "<a class='btn btn-primary btn-block' style='color: white!important;' href='index.html?filter_date=" + pickup.date + "&search=" + pickup.number + "&tab=tab_pickups'> <i class='fa fa-external-link'></i> Ver </a><br>" +
                             "<a class='a-popup-close-button btn btn-danger btn-block' style='color: white!important;' href='#close'> <i class='fa fa-times'></i> Ocultar </a><br>"
                         );
@@ -121,19 +121,19 @@ function initializePage() {
                 var App_= this;
                 var url_params= UrlUtility_.getParams();
                 $.each(deliveries, function (index, delivery) {
-                    if(delivery.longitude !== null && delivery.latitude !== null){
-                        var icon = App_.new_icon('images/map_icons/delivery_' + delivery.delivery_state.class + '.png?1');
+                    if(delivery.long !== null && delivery.lat !== null){
+                        var icon = App_.new_icon('images/map_icons/delivery_'+Settings.delivery_state[delivery.state_id].class+'.png?1');
 
                         var marker= new L.marker().setLatLng({
-                            lng: delivery.longitude,
-                            lat: delivery.latitude
+                            lng: delivery.long,
+                            lat: delivery.lat
                         }).setIcon(icon).addTo(App_.map).bindPopup(
-                            "<div style='text-align:center;'>Entrega <span class='label bg-" + delivery.delivery_state.class + "'>" + delivery.delivery_state.name + "</span></div>" +
-                            "<b>" + delivery.delivery_number + "</b><br>" +
+                            "<div style='text-align:center;'>Entrega <span class='label bg-"+Settings.delivery_state[delivery.state_id].class+"'>"+Settings.delivery_state[delivery.state_id].name+"</span></div>" +
+                            "<b>" + delivery.number + "</b><br>" +
                             "<b>Dirección: </b> " + delivery.address + " <br>" +
                             "<b>Observaciones dirección: </b> " + delivery.long_address + "<br>" +
                             "<i class='fa fa-clock-o'></i>" + delivery.start_time + " y " + delivery.end_time + " <br>"+
-                            "<i class='fa fa-globe'></i> A "+delivery.distance_in_mts+"  <br>"+
+                            "<i class='fa fa-globe'></i> A "+Haversine.mtrs_to_text(delivery.distance_in_mts)+"  <br>"+
                             "<a class='btn btn-primary btn-block' style='color: white!important;' href='index.html?filter_date="+delivery.date+"&search="+delivery.number+"&tab=tab_deliveries'> <i class='fa fa-external-link'></i> Ver </a><br>"+
                             "<a class='a-popup-close-button btn btn-danger btn-block' style='color: white!important;' href='#close'> <i class='fa fa-times'></i> Ocultar </a><br>"
                         );
