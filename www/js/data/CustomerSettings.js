7562830d76870b4d8b4c80e9a6e6ebe5de70de18
oscarricardosan <oscarricardosan@gmail.com>
2018-01-04 15:55:55 -0500

    Se agrega como cliente a notijudiciales

--- www/js/data/CustomerSettings.js
+++ www/js/data/CustomerSettings.js
@@ -1,6 +1,9 @@
 var CustomerSettings= [
     {
         domain: "demo.savne.net",
-        url_server: 'https://courier-app.savne.net',
+        url_server: 'https://courier-app.savne.net'
+    }, {
+        domain: "demo.notijudicial.com",
+        url_server: 'https://operaciones.notijudicial.com'
     }
 ];
