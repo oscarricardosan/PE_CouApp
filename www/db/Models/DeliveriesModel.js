1a53768faf8cab29b32163abde2220617f3644c6
oscarricardosan <oscarricardosan@gmail.com>
2018-01-05 16:25:25 -0500

    Ajuste para evitar colición en las notificaciones

--- www/db/Models/DeliveriesModel.js
+++ www/db/Models/DeliveriesModel.js
@@ -57,7 +57,7 @@ var DeliveriesModel= (function () {
     var insertOrUpdateById = function(data_, callback_){
         callback_= PolishedUtility_.callback(callback_);
         find({id: data_.id}, {success: function(tx, results){
-            console.log(results);
+            alert(JSON.stringify(results));
             if(results._number_rows > 0)
                 update({id: data_.id}, data_, callback_);
             else
